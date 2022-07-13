87c2fa1cd6ffc49feaf091cc0b242620d50d9bc6
kierhb <kierbobila@gmail.com>
2022-07-13 15:36:53 +0800

    Practice using if...else Statements

---
+++ grades-ifelse.js
@@ -0,0 +1,26 @@
+let grade = prompt("Please enter grade: ", "");
+
+//If grade is 90 or greater
+if (grade >= 90) {
+    console.log("A");
+}
+
+//If grade is 80 or greater
+else if (grade >= 80) {
+    console.log("B");
+}
+
+//If grade is 70 or greater
+else if (grade >= 70) {
+    console.log("C");
+}
+
+//If grade is 60 or greater
+else if (grade >= 60) {
+    console.log("D");
+}
+
+//If grade is below 60
+else {
+    console.log("E");
+}
